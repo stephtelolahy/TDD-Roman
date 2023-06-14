33be649aed5d2844352659d57c5721a24ac5a255
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-06-14 22:58:44 +0200

    Initial Commit

---
+++ Roman/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Roman
+//
+//  Created by Hugues Telolahy on 14/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Roman/RomanApp.swift
@@ -0,0 +1,17 @@
+//
+//  RomanApp.swift
+//  Roman
+//
+//  Created by Hugues Telolahy on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RomanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
