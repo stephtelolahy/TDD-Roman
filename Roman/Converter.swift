c60b40cfb79f7e3f2455813149be1912158c9eac
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-06-14 23:08:20 +0200

    Initial commit

---
+++ Roman/Converter.swift
@@ -0,0 +1,15 @@
+//
+//  Converter.swift
+//  Roman
+//
+//  Created by Hugues Telolahy on 14/06/2023.
+//
+
+public class Converter {
+
+    public init() {}
+
+    public func convert(_ number: Int) -> String {
+        ""
+    }
+}
--- RomanTests/RomanTests.swift
+++ RomanTests/RomanTests.swift
@@ -6,30 +6,18 @@
 //
 
 import XCTest
+import Roman
 
 final class RomanTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testConversionForOne() {
+        // Given
+        let converter = Converter()
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+        // When
+        let result = converter.convert(1)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        // Then
+        XCTAssertEqual(result, "I")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
